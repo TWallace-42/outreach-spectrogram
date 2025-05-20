665a287ba13a2cb9c8aec6e402981bca11f8a68b
Thomas Wallace <t.wallace.1@research.gla.ac.uk>
2025-05-20 15:05:55 +0100

    Tidied injection maker.py

--- README.md
+++ README.md
@@ -1,9 +1,40 @@
 # Outreach Spectrogram
 A very basic spectrogram that, in theory, has room to grow and improve.
-In fact it has a very large amount of room. Any questions or issues please
+In fact, it has a very large amount of room. Any questions or issues please
 email Thomas Wallace at t.wallace.1@research.gla.ac.uk.
 
 This README will be a very basic guide into the usage and todo list of the repo
 as well as some basic troubleshooting.
+
+The main goal of this project is to have a real time spectrogram that we can 
+have output standard audio inputs to a raspberry-pi3. Additionally it would
+be nice if we could show injections.
+
+Below I will detail each individual file, how it's run, and then at the bottom a large TODO list
+
+# run_spectrogram.py
+
+# injection_maker.py
+This turns standard png/jpg/jpeg images into txt arrays that can be injected into the spectrogram.
+
+to run in the command line you can try:
+
+```
+python injection_maker.py <file_name>.png/.jpg/.jpeg 
+```
+
+There are various options but the main one is the flag `-i` which will invert the grayscale image.
+
+# pyaudio_device_list.py
+
+
+# TODO
+[ ] Finish README.md
+[x] Tidy injection_maker.py
+[x] Make injection_maker.py run in command line
+[ ] Injection Sound File maker
+[ ] Figure out why there's strange low frequency artifacts
+[ ] Calculate FFT frequencies to display
+[ ] log colormap
 
 
--- injection_maker.py
+++ injection_maker.py
@@ -1,28 +1,56 @@
 import numpy as np
 from PIL import Image, ImageOps
 import matplotlib.pyplot as plt
-#freq = 30
+import os
+from argparse import ArgumentParser
 
-#spectrogram_size = (150,100) #freqs, length
-#injection = np.zeros(spectrogram_size)
-#injection[freq] = 1.
-#np.savetxt("injections/test.txt",injection)
 
-def make_igr(invert = False):
+def create_dir(*PATHS):
+	for p in PATHS:
+		components = p.split("/")
+		for i,_ in enumerate(components):
+			new_dir = "/".join(components[:i+1]) + "/"
+			if not os.path.isdir(new_dir):
+				os.makedirs(new_dir)
+				print(f"Created: {new_dir}")
+
+def make_injection(file_name,invert = False,spectrogram_size = (150,150)):
+	#the size of image on the spectrogram,
+	# if I remember correctly the size is set to the same as the spectrogram in 
+	# run_spectrogram.py so that might need hardcoded
+	create_dir("injections/")
 	spectrogram_size = (150,150) 
-	logo = Image.open("IGRlogo_black.png").resize((int(spectrogram_size[0]),int(spectrogram_size[1]))).transpose(Image.FLIP_TOP_BOTTOM)
-
+	#load image
+	logo = Image.open(file_name).resize((int(spectrogram_size[0]),int(spectrogram_size[1]))).transpose(Image.FLIP_TOP_BOTTOM)
+	#greyscale
 	grey_logo = ImageOps.grayscale(logo)
-	logo_arr = np.array(logo)/255 #use grey_logo if not transparency issues
+	
+	logo_arr = np.array(logo)/255 
+	#use grey_logo if not transparency issues
 	if len(logo_arr.shape) > 2:
-		logo_arr = logo_arr[:,:,3]
+		
+		logo_arr = logo_arr[:,:,0]
 	
 	if invert:
 		logo_arr = 1-logo_arr
-		savename = "injections/igr_invert2.txt"
-	else:
-		savename = "injections/igr2.txt"
+	savename = f"injections/{file_name.replace('.jpeg','.txt').replace('.png','.txt')}"
+
+		
+	#save
 	np.savetxt(savename,logo_arr)
-	
-make_igr()
-make_igr(True)
+	print(f"injection saved in {savename}")
+if __name__ == "__main__":
+	parser = ArgumentParser(prog = "injection_maker.py",
+			description = "Creates injection arrays for inputing to the spectrogram",
+			epilog = "If there are any questions and comments please email t.wallace.1@research.gla.ac.uk")
+	parser.add_argument("filename",help = "file to turn into an injection")
+	parser.add_argument("-i","--invert",help = "Whether to invert the values of the array",
+	action = "store_true")
+	parser.add_argument("-s","--spec_size",
+	help = "Size of spectrogram, by default will assume the injection should be square",
+	type = int,default = 150)
+	args = parser.parse_args()
+	make_injection(file_name = args.filename,
+				invert = args.invert,
+				spectrogram_size = (args.spec_size,args.spec_size))
+				
